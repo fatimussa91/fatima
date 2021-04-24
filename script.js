3ce866db9eb5552900881e52ef27327e5bf12331
Fatimamuss <49186417+Fatimamuss@users.noreply.github.com>
2021-04-24 22:51:48 +0200

    mod.

---
+++ script.js
@@ -0,0 +1,7 @@
+function pasuser(form) {
+if (form.uname.value=="hallofa" && form.psw.value=="3920-fa") {             
+location="myResume.html" 
+} else {
+alert("Invalid Username or Password")
+}
+}
