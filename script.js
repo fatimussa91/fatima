27c858a7cdf3cd183f9d04225ec37269a5f7e61f
Fatimamuss <49186417+Fatimamuss@users.noreply.github.com>
2021-05-17 15:22:20 +0200

    mod.

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-/*
-function pasuser(form) {
-if (form.uname.value=="hallofa" && form.psw.value=="3920-fa") {             
-location="myResume.html" 
-} else {
-alert("Invalid Username or Password")
-}
-}
-*/
+     
+		
+
+		var add = document.getElementById('add').onclick = function(){
+
+		       var wert1 = document.getElementById('wert1').value;
+			   var wert2 = document.getElementById('wert2').value;  
+
+		    	var result = parseFloat(wert1) + parseFloat(wert2);
+
+		    	document.getElementById('result1').value = result;     
+		}
+
+	var sub =  document.getElementById('sub').onclick = function(){
+
+		       var wert1 = document.getElementById('wert3').value;
+			   var wert2 = document.getElementById('wert4').value;  
+
+		    	var result = parseFloat(wert1) - parseFloat(wert2);
+
+		    	document.getElementById('result2').value = result;    
+		}
+
+	var mult =  document.getElementById('mult').onclick = function(){
+
+		       var wert1 = document.getElementById('wert5').value;
+			   var wert2 = document.getElementById('wert6').value;  
+
+		    	var result = parseFloat(wert1) * parseFloat(wert2);
+
+		    	document.getElementById('result3').value = result;    
+		}
+
+	var div =   document.getElementById('div').onclick = function(){
+
+		       var wert1 = document.getElementById('wert7').value;
+			   var wert2 = document.getElementById('wert8').value;  
+
+		    	var result = parseFloat(wert1) / parseFloat(wert2);
+
+		    	document.getElementById('result4').value = result;    
+		}
+
+		
+    
