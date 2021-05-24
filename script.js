548beebc725cf3a430d2ecc9df70efd066c02ed3
Fatimamuss <49186417+Fatimamuss@users.noreply.github.com>
2021-05-24 11:50:27 +0200

    mod.

--- script.js
+++ script.js
@@ -116,40 +116,62 @@
 
 	    		return output;
 	    }*/
-	    function isprim(){
-	    	num = document.getElementById("nr").value;
-	    	for(var n = 0; n <=num; n++){
 
-	    		console.log(checkForPrime());
 
-	    	}
-	    }
+	    function isprime(){
+	    	var	num = document.getElementById("nr").value;
 
-	   function checkForPrime() {
-			num = document.getElementById("nr").value;
-
-			if(num == '' || num < 1) {
+	   		if(num == '' || num < 1) {
 				document.getElementById("result").innerHTML = "Bitte geben Sie eine gültige Zahl an!";
 				return;
-				}
-			
 				
-				var i;
-				var flag = true;
+			}
+			else
+			{
+
+				var primArray = [];
+
+	    	for(var n = 1; n <= num; n++){
+
+			var isPrimNum =checkIfPrime(n);
+
+	    		if(isPrimNum){
+	    			primArray.push(isPrimNum);
+	    		}
+
+	    		
+	    		
+	    			document.getElementById("result").innerHTML ="Die Primzahlen zwichen 1 und " + num + " sind : &#40; " + primArray + " &#41;";
+	    			
+	    
+	    	
+	    	}
+	    		}	
+	    }
+
+	   function checkIfPrime(n) {
+			var num = n;
+			var i;
+			var flag = true;
+
 				if (num == 1) {
-				flag = false;
-				} else {
-				for(i = 2; i < num-1; i++) {
-				if (num % i == 0) {
-				flag = false;
-				break;
-				}
-				}
-				}
+
+					flag = false;
+
+				} else{
+						for(i = 2; i < num-1; i++) {
+							if (num % i == 0) {
+							flag = false;
+							break;
+							}
+						 }
+					}
+
 				if(flag == true) {
-				document.getElementById("result").innerHTML = "Die Zahl: " + num + " ist eine Primzahl!";
+					document.getElementById("result").innerHTML = "Die Zahl: " + num + " ist eine Primzahl!";
+					return num;
 				} else {
-				document.getElementById("result").innerHTML = "Die Zahl: " + num + " ist keine Primzahl!";
+					flag = false;
 				}
 			
 		}
